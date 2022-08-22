a4cb6d2936c3408af8025751ccbde9048aa3ba1e
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-08-22 18:58:49 +0300

    Add navebarMenu protal to icons menu

--- src/components/navbar/NavbarIcons.js
+++ src/components/navbar/NavbarIcons.js
@@ -3,6 +3,9 @@ import { IoSearch } from "react-icons/io5";
 import { HiOutlineUser } from "react-icons/hi";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { IoCloseOutline } from "react-icons/io5";
+import { useState } from "react";
+import NavbarMenu from "./NavbarMenu";
 
 const NavbarIcons = () => {
   const items = [
@@ -22,6 +25,9 @@ const NavbarIcons = () => {
       id: 3,
     },
   ];
+
+  const [show, setShow] = useState(false);
+
   return (
     <ul className={styles["nav"]}>
       <li className={`${styles["body-1"]} ${styles["nav__link"]}`}>Language</li>
@@ -35,9 +41,11 @@ const NavbarIcons = () => {
       ))}
       <li
         className={`${styles["icon"]} ${styles["nav__link"]} ${styles["nav__menu"]}`}
+        onClick={() => setShow(!show)}
       >
-        <HiOutlineMenuAlt3 />
+        {show ? <IoCloseOutline /> : <HiOutlineMenuAlt3 />}
       </li>
+      <NavbarMenu state={show ? "open" : "close"} />
     </ul>
   );
 };
