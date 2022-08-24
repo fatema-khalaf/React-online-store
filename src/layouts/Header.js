9f3ee06e60c49ac6b2f8bf45299ae18b6bd0176f
fatema-khalaf <www.fatemakhalaf2015@gmail.com>
2022-08-24 22:03:29 +0300

    header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,6 +2,7 @@ import NavbarList from "../components/navbar/NavbarList";
 import styles from "../theme/main.module.scss";
 import logo from "../logo3.png";
 import NavbarIcons from "../components/navbar/NavbarIcons";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const items = [
@@ -11,8 +12,40 @@ const Header = () => {
     "item test",
     "item test",
   ];
+  const [back, setBack] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+  //   componentDidMount: function() {
+  //     window.addEventListener('scroll', this.handleScroll);
+  // },
+
+  // componentWillUnmount: function() {
+  //     window.removeEventListener('scroll', this.handleScroll);
+  // },
+  // const handleScroll = (event) => {
+  //   console.log("scrollTop: ", event.currentTarget.scrollTop);
+  //   console.log("offsetHeight: ", event.currentTarget.offsetHeight);
+  // };
+  const handleScroll = (event) => {
+    // let scrollTop = event.srcElement.body.scrollY;
+    // // itemTranslate = Math.min(0, scrollTop / 3 - 60);
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    console.log(document.documentElement.clientHeight);
+    console.log(winScroll - 100);
+    console.log(winScroll > document.documentElement.clientHeight - 100);
+    if (winScroll > document.documentElement.clientHeight - 100) {
+      setBack(true);
+    }
+    if (winScroll < document.documentElement.clientHeight - 100) {
+      setBack(false);
+    }
+  };
+  // const onscroll = (e) => {};
+  console.log(back);
   return (
-    <header className={styles["header"]}>
+    <header className={`${styles["header"]} ${back && styles["header--2"]}`}>
       <div className={styles["logo"]}>
         <img src={logo} className={styles["logo__img"]} />
       </div>
